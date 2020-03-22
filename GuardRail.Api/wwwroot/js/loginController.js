2b52d8224efb8ae76e10023021429df38fdc5935
Joshua Galloway <joshuaquiz@gmail.com>
2020-03-22 16:25:02 -0400

    Added an auth UI, still needs a backend lol

---
+++ GuardRail.Api/wwwroot/js/loginController.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+app.controller(
+    "loginController",
+    [
+        "$scope",
+        "$rootScope",
+        "$location",
+        "authenticationService",
+        function ($scope, $rootScope, $location, authenticationService) {
+            $rootScope.hideMenus = $routeParams.hideMenus;
+            authenticationService.ClearCredentials();
+            $scope.login = () => {
+                $scope.dataLoading = true;
+                authenticationService.Login(
+                    $scope.email,
+                    $scope.password,
+                    response => {
+                        if (response.success) {
+                            authenticationService.SetCredentials(
+                                $scope.email,
+                                $scope.password);
+                            $location.path("/");
+                        } else {
+                            $scope.error = response.message;
+                            $scope.dataLoading = false;
+                        }
+                    });
+            };
+        }
+    ]);
--- GuardRail.Api/wwwroot/js/main.js
+++ GuardRail.Api/wwwroot/js/main.js
@@ -3,7 +3,8 @@
 var app = angular.module(
     "app",
     [
-        "ngRoute"
+        "ngRoute",
+        "ngCookies"
     ])
     .factory(
         "connection",
@@ -16,30 +17,143 @@ var app = angular.module(
                 return () => { };
             }
         ])
+    .factory(
+        "base64",
+        () => {
+            var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+            return {
+                encode: input => {
+                    var output = "";
+                    var chr1, chr2, chr3;
+                    var enc1, enc2, enc3, enc4;
+                    var i = 0;
+                    do {
+                        chr1 = input.charCodeAt(i++);
+                        chr2 = input.charCodeAt(i++);
+                        chr3 = input.charCodeAt(i++);
+
+                        enc1 = chr1 >> 2;
+                        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                        enc4 = chr3 & 63;
+
+                        if (isNaN(chr2)) {
+                            enc3 = enc4 = 64;
+                        } else if (isNaN(chr3)) {
+                            enc4 = 64;
+                        }
+
+                        output = output +
+                            keyStr.charAt(enc1) +
+                            keyStr.charAt(enc2) +
+                            keyStr.charAt(enc3) +
+                            keyStr.charAt(enc4);
+                        chr1 = chr2 = chr3 = "";
+                        enc1 = enc2 = enc3 = enc4 = "";
+                    } while (i < input.length);
+                    return output;
+                },
+                decode: input => {
+                    var output = "";
+                    var chr1, chr2, chr3;
+                    var enc1, enc2, enc3, enc4;
+                    var i = 0;
+                    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+                    var base64Test = /[^A-Za-z0-9+/=]/g;
+                    if (base64Test.exec(input)) {
+                        window.alert("There were invalid base64 characters in the input text.\n" +
+                            "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                            "Expect errors in decoding.");
+                    }
+                    input = input.replace(/[^A-Za-z0-9+/=]/g, "");
+                    do {
+                        enc1 = keyStr.indexOf(input.charAt(i++));
+                        enc2 = keyStr.indexOf(input.charAt(i++));
+                        enc3 = keyStr.indexOf(input.charAt(i++));
+                        enc4 = keyStr.indexOf(input.charAt(i++));
+
+                        chr1 = (enc1 << 2) | (enc2 >> 4);
+                        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                        chr3 = ((enc3 & 3) << 6) | enc4;
+
+                        output = output + String.fromCharCode(chr1);
+
+                        if (enc3 !== 64) {
+                            output = output + String.fromCharCode(chr2);
+                        }
+                        if (enc4 !== 64) {
+                            output = output + String.fromCharCode(chr3);
+                        }
+
+                        chr1 = chr2 = chr3 = "";
+                        enc1 = enc2 = enc3 = enc4 = "";
+
+                    } while (i < input.length);
+                    return output;
+                }
+            };
+        })
+    .factory(
+        "authenticationService",
+        [
+            "base64",
+            "$http",
+            "$cookies",
+            "$rootScope",
+            function (base64, $http, $cookies, $rootScope) {
+                return {
+                    Login: (email, password, callback) =>
+                        $http.post(
+                            "/api/authenticate",
+                            {
+                                email: email,
+                                password: password
+                            })
+                            .success(callback),
+                    SetCredentials: (email, password) => {
+                        var authData = base64.encode(`${email}:${password}`);
+                        $rootScope.globals = {
+                            currentUser: {
+                                email: email,
+                                authData: authData
+                            }
+                        };
+                        $http.defaults.headers.common["Authorization"] = `Basic ${authData}`;
+                        $cookies.put("globals", $rootScope.globals);
+                    },
+                    ClearCredentials: () => {
+                        $rootScope.globals = {};
+                        $cookies.remove("globals");
+                        $http.defaults.headers.common.Authorization = "Basic ";
+                    }
+                }
+            }])
     .config(
         [
             "$routeProvider",
             $routeProvider => {
                 $routeProvider
                     .when(
+                        "/login",
+                        {
+                            templateUrl: "/pages/login.html",
+                            controller: "loginController"
+                        })
+                    .when(
                         "/home",
                         {
                             templateUrl: "/pages/home.html",
-                            controller: "homeController",
-                            controllerAs: "home"
+                            controller: "homeController"
                         })
                     .when(
                         "/users",
                         {
                             templateUrl: "/pages/users.html",
-                            controller: "usersController",
-                            controllerAs: "users"
+                            controller: "usersController"
                         })
                     .otherwise(
                         {
-                            templateUrl: "/pages/home.html",
-                            controller: "homeController",
-                            controllerAs: "home"
+                            redirectTo: "/login"
                         });
             }
         ])
@@ -48,15 +162,24 @@ var app = angular.module(
         [
             "$rootScope",
             "$scope",
-            "$route",
+            "$http",
+            "$cookies",
             "$location",
-            "$routeParams",
             "connection",
-            ($rootScope, $scope, $route, $location, $routeParams, connection) => {
+            ($rootScope, $scope, $http, $cookies, $location, connection) => {
                 connection();
-                $rootScope.$on("message", d => console.log(d));
-                $scope.$route = $route;
-                $scope.$location = $location;
-                $scope.$routeParams = $routeParams;
+                $rootScope.globals = $cookies.get("globals") || {};
+                if ($rootScope.globals.currentUser) {
+                    $http.defaults.headers.common["Authorization"] = `Basic ${$rootScope.globals.currentUser.authdata}`;
+                }
+                $rootScope.$on("$locationChangeStart", (event, next, current) => {
+                    // redirect to login page if not logged in
+                    if ($location.path() !== "/login" && !$rootScope.globals.currentUser) {
+                        $location.path("/login");
+                        $scope.showMenus = true;
+                    } else {
+                        $scope.showMenus = false;
+                    }
+                });
             }
         ]);
